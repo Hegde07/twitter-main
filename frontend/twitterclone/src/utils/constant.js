2130a85e06894cd5a146930bcf2c6bdfc1eae666
Hegde07 <hegdebhargav92@gmail.com>
2024-07-04 14:09:10 -0700

    Modified

--- frontend/twitterclone/src/utils/constant.js
+++ frontend/twitterclone/src/utils/constant.js
@@ -1,5 +1,5 @@
 export const USER_API_END_POINT = "https://twitter-main.vercel.app/api/v1/user";
-export const TWEET_API_END_POINT = "https://twitter-main.vercel.app/v1/tweet";
+export const TWEET_API_END_POINT = "https://twitter-main.vercel.app/api/v1/tweet";
 
 export const timeSince = (timestamp) => {
     let time = Date.parse(timestamp);
